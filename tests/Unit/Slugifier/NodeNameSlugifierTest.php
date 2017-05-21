8b70ad852b903f091f34828f503ee05a9dfb509d
Yury Buldakov <yury.buldakov@gmail.com>
2017-05-21 12:30:21 +0300

    Transliterating slugifier to support non-latin chars
    * extend unit test to unhide the problem
    * StyleCI code styling fix

--- tests/Unit/Slugifier/NodeNameSlugifierTest.php
+++ tests/Unit/Slugifier/NodeNameSlugifierTest.php
@@ -12,10 +12,17 @@
 namespace Sulu\Component\DocumentManager\tests\Unit\Slugifier;
 
 use Sulu\Component\DocumentManager\Slugifier\NodeNameSlugifier;
+use Sulu\Component\DocumentManager\Tests\Bootstrap;
 use Symfony\Cmf\Api\Slugifier\SlugifierInterface;
+use Symfony\Component\DependencyInjection\Container;
 
 class NodeNameSlugifierTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Container
+     */
+    private $container;
+
     /**
      * @var SlugifierInterface
      */
@@ -28,33 +35,50 @@ class NodeNameSlugifierTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        $this->slugifier = $this->prophesize(SlugifierInterface::class);
-        $this->nodeNameSlugifier = new NodeNameSlugifier($this->slugifier->reveal());
+        $this->container = Bootstrap::createContainer();
+        $this->slugifier = $this->container->get('sulu_document_manager.slugifier');
+        $this->nodeNameSlugifier = new NodeNameSlugifier($this->slugifier);
     }
 
     public function testSlugify()
     {
-        $this->slugifier->slugify('Test article')->willReturn('test-article');
-
         $this->assertEquals('test-article', $this->nodeNameSlugifier->slugify('Test article'));
+    }
+
+    public function testSlugifyLatinExtended()
+    {
+        $this->assertEquals('rozszerzony-lacinska', $this->nodeNameSlugifier->slugify('Rozszerzony łacińska'));
+    }
+
+    public function testSlugifyNonLatin()
+    {
+        // ukrainian cyrillic
+        $this->assertEquals('testova-stattia-z-i-yi-ie-g', $this->nodeNameSlugifier->slugify('Тестова стаття з і, ї, є, ґ'));
+        // japanese
+        $this->assertEquals('tesutoji-shi', $this->nodeNameSlugifier->slugify('テスト記事'));
+        // chinese (simplified)
+        $this->assertEquals('ce-shi-wen-zhang', $this->nodeNameSlugifier->slugify('测试文章'));
+        // russian cyrillic
+        $this->assertEquals('testovaia-statia-s-io-y', $this->nodeNameSlugifier->slugify('Тестовая статья с ё, ъ, ы'));
     }
 
     public function provide10eData()
     {
         return [
             ['10e', '10-e'],
-            ['.10e', '.10-e'],
-            ['-10e', '-10-e'],
-            ['%10e', '%10-e'],
+            ['.10e', '10-e'],
+            ['-10e', '10-e'],
+            ['%10e', '10-e'],
             ['test-10e-name', 'test-10-e-name'],
-            ['test.10e-name', 'test.10-e-name'],
-            ['test%10e-name', 'test%10-e-name'],
-            ['test-10E-name', 'test-10-E-name'],
-            ['test.10E-name', 'test.10-E-name'],
-            ['test%10E-name', 'test%10-E-name'],
-            ['test10E-name', 'test10-E-name'],
+            ['test.10e-name', 'test-10-e-name'],
+            ['test%10e-name', 'test-10-e-name'],
+            ['test-10E-name', 'test-10-e-name'],
+            ['test.10E-name', 'test-10-e-name'],
+            ['test%10E-name', 'test-10-e-name'],
+            ['test10E-name', 'test10-e-name'],
             ['test-9e-name', 'test-9-e-name'],
             ['test-500e-name', 'test-500-e-name'],
+            ['тестова-500e-назва', 'testova-500-e-nazva'],
         ];
     }
 
@@ -63,8 +87,6 @@ class NodeNameSlugifierTest extends \PHPUnit_Framework_TestCase
      */
     public function testSlugify10e($actual, $expected)
     {
-        $this->slugifier->slugify($actual)->willReturn($actual);
-
         $this->assertEquals($expected, $this->nodeNameSlugifier->slugify($actual));
     }
 }
--- tests/Unit/Subscriber/Phpcr/FindSubscriberTest.php
+++ tests/Unit/Subscriber/Phpcr/FindSubscriberTest.php
@@ -18,11 +18,9 @@ use Sulu\Component\DocumentManager\Event\HydrateEvent;
 use Sulu\Component\DocumentManager\Events;
 use Sulu\Component\DocumentManager\Exception\DocumentManagerException;
 use Sulu\Component\DocumentManager\Metadata;
-use Sulu\Component\DocumentManager\MetadataFactory;
 use Sulu\Component\DocumentManager\MetadataFactoryInterface;
 use Sulu\Component\DocumentManager\NodeManager;
 use Sulu\Component\DocumentManager\Subscriber\Phpcr\FindSubscriber;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class FindSubscriberTest extends \PHPUnit_Framework_TestCase
